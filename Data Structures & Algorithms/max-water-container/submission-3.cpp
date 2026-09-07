ae0487db291fa56867240400d10d71c444ba5ab2
PeterSampsonT <petersampsont@gmail.com>
2026-09-07 16:55:44 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+class Solution {
+public:
+    int maxArea(const vector<int>& heights) {
+        int max_vol = 0;
+        int i = 0, j = heights.size() - 1;
+
+        while (i < j) {
+            int h_left = heights[i];
+            int h_right = heights[j];
+            int h = min(h_left, h_right);
+
+            max_vol = max(max_vol, (j - i) * h);
+
+            while (i < j && heights[i] <= h) i++;
+            while (i < j && heights[j] <= h) j--;
+        }
+
+        return max_vol;
+    }
+};
